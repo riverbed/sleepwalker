f0a0311e84a91d16c7ec402ea2c4da62d15dfd33
Henry Andrews <handrews@riverbed.com>
2014-01-11 21:00:55 -0800

    Rework packaging for use with internal pypibox.
    This moves sleepwalker over to the system used by the PQ infrastructure.
    This is mostly for internal packaging, and some further work needs to be
    done to sort out open-source packaging.

---
+++ README.rst
@@ -0,0 +1,6 @@
+sleewpalker
+===========
+
+``sleewpalker`` is a client package for REST APIs that are implemented
+using ``reschema``-based JSON schemas.
+
--- setup.py
+++ setup.py
@@ -1,23 +1,51 @@
+import os
+import pip
+import sys
+
+from setuptools.command.test import test as TestCommand
+from pip.req import parse_requirements
+
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-from contrib.version import get_git_version
+def requirements():
+    return [str(ir.req) for ir in parse_requirements('requirements.txt')]
+    
+def get_version():
+    try:
+        # pip-installed packages can get the version from the 'version.txt'
+        # file, since it is included in the package MANIFEST.
+        with open('version.txt', 'r') as f:
+            return f.read().strip()
+    except IOError:
+        # since 'version.txt' is .gitignored, running setup.py (install|develop)
+        # from a git repo requires a bit of bootstrapping. in this case, we use
+        # the raw .git tag as the version.
+        pip.main(['install', '-U',
+                  '-i http://pypi.lab.nbttech.com/pq/development/', 'pq-ci'])
+        from pq_ci import git
+        tag = git.parse_tag()
+        return '-'.join([tag['version'], tag['commits'], tag['sha']])
 
-setup(name="sleepwalker",
-      version=get_git_version(),
-      description="sleepwalker - Interact with REST-servers using reschema-based schemas",
-      author="Riverbed Technology",
-      author_email="cwhite@riverbed.com",
-      
-      packages=find_packages(),
-      scripts=[
-      ],
-      install_requires=[
-          "requests>=1.2.3",
-          "uritemplate>=0.6",
-          "jsonpointer",
-          "reschema",
-      ],
-      include_package_data=True)
+readme = open('README.rst').read()
+
+setup(
+    name='sleepwalker',
+    version=get_version(),
+    description="sleepwalker - Interact with REST-servers using reschema-based schemas",
+    long_description=readme,
+    author="Riverbed Technology",
+    author_email="cwhite@riverbed.com",
+    packages=[
+        'sleepwalker',
+    ],
+    package_dir={'sleepwalker': 'sleepwalker'},
+    scripts=[
+    ],
+    include_package_data=True,
+    install_requires=requirements(),
+    keywords='sleepwalker',
+    tests_require=['pytest', 'mock'],
+)
