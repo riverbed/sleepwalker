efb0cfe8845e7b4fd4f8812cb3553f0b388c0c50
Christoher J. White <cwhite@riverbed.com>
2014-07-15 14:43:31 -0700

    Update versioning to latest

--- versioning/__init__.py
+++ versioning/__init__.py
@@ -1,14 +1,8 @@
 # Copyright (c) 2014 Riverbed Technology, Inc.
 #
-# This software is licensed under the terms and conditions of the
-# MIT License set forth at:
-#   https://github.com/riverbed/sleepwalker/blob/master/LICENSE ("License").
-# This software is distributed "AS IS" as set forth in the License.
-
-from __future__ import unicode_literals, print_function, division
-import re
-from subprocess import Popen, PIPE
-import os
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
 
 """
 This module contains code for interacting with git repositories. It is
@@ -16,13 +10,25 @@ used to determine an appropriate version number from either the local
 git repository tags or from a version file.
 """
 
-def verify_repository():
-    """Raise an error if this source file is not in tracked by git."""
+from __future__ import unicode_literals, print_function, division
+import os
+import inspect
+from subprocess import Popen, PIPE
 
-    f = os.path.dirname(os.path.abspath(__file__))
-    process = Popen(['git', 'ls-files', f, '--error-unmatch'],
-                    stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
+
+def verify_repository(pkg_file):
+    """Raise an error if this source file is not in tracked by git."""
+    dirname = os.path.dirname(pkg_file)
+    basename = os.path.basename(pkg_file)
+
+    cwd = os.getcwd()
+    try:
+        os.chdir(dirname)
+        process = Popen(['git', 'ls-files', basename, '--error-unmatch'],
+                        stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+    finally:
+        os.chdir(cwd)
 
     if stderr:
         # Not a git repo
@@ -103,29 +109,49 @@ def parse_tag():
             'sha': sha.strip(), }
 
 
-def get_version(v_file='RELEASE-VERSION'):
-    """Return <tag>.dev<commits> style version string.
+def get_version(pkg_file=None, v_file='RELEASE-VERSION'):
+    """Return <tag>.post<commits> style version string.
+
+    :param pkg_file: Some filename in the package, used to test if this
+       is a live git repostitory (defaults to caller's file)
 
     :param v_file: Fallback path name to a file where release_version is saved
     """
 
+    if pkg_file is None:
+        parent_frame = inspect.stack()[1]
+        pkg_file = inspect.getabsfile(parent_frame[0])
+
     try:
-        verify_repository()
+        verify_repository(pkg_file)
         git_info = parse_tag()
         branch = get_branch()
-        assert branch == 'master'
-        if git_info['commits'] == '0':
-            version = git_info['version']
+        if branch == 'master':
+            if git_info['commits'] == '0':
+                version = git_info['version']
+            else:
+                version = '%s.post%s' % (git_info['version'], git_info['commits'])
         else:
-            version = '%s.dev%s' % (git_info['version'], git_info['commits'])
+            # Still allow building of the package, but give it
+            # a different version.  The use of g_ ensures that
+            # python will give it the proper ordering after final
+            # but before post:
+            #   6.0.0                   -> 6, 0, 0, *final
+            #   6.0.0-g_branch-34-hash  -> 6, 0, 0, *g_branch, 34 hash
+            #   6.0.0-post34            -> 6, 0, 0, *post, 34
+            version = '%s-g_%s-%s-%s' % (
+                git_info['version'], branch, git_info['commits'], git_info['sha'])
 
         with open(v_file, 'w') as f:
             f.write(version)
 
     except EnvironmentError:
         # Not a git repository, so fall back to reading RELEASE-VERSION
-        with open(v_file, 'r') as f:
-            version = f.read()
+        if (os.path.exists(v_file)):
+            with open(v_file, 'r') as f:
+                version = f.read().strip()
+        else:
+            version = 'unknown'
 
     except AssertionError:
         print('Release version string can only be derived from master branch.'
